041d9dccb645a0393050a27e289cb3c901c8a00a
Nat Wilson <njwilson23@gmail.com>
2014-04-30 14:56:18 -0400

    FIX: forgot to update state.temp in the previous commit

--- Iceberg.jl
+++ Iceberg.jl
@@ -90,8 +90,9 @@ function tsolve!(state::ModelState1D, phys::PhysicalParams)
     alphal = phys.kapl / (phys.cpl * phys.rhol)
     alpha = [state.phi[i] < 0.0 ? alphal : alphas for i=1:state.params.nx[1]]
 
-    dT = Array(Float64, stat.params.nx)
+    dT = Array(Float64, state.params.nx)
     dT[:] = state.params.dt / state.params.dx[1]^2 * alpha .* (L * state.temp[:])
+    state.temp += dT
     return state.temp
 end
 
