84d8d76d137f6460bade28f7d3eea58e441942a7
Nat Wilson <njwilson23@gmail.com>
2014-05-02 21:58:55 -0400

    add test for test_front_indices for the 1d case

--- tests.jl
+++ tests.jl
@@ -12,12 +12,32 @@ function test_initialize1d()
 end
 
 function test_interp1d()
+    println("test_interp1d")
+
     xp = [0, 5, 7, 8, 12]
     yp = [0, 2, 6, 3, 7]
     xi = [2.5, 6, 7.5, 11]
     y_ans = [1, 4, 4.5, 6]
     yi = Iceberg.interp1d(xi, xp, yp)
     @test y_ans == yi
+end
+
+function test_front_indices()
+    # single front
+    println("test_front_indices")
+    state = ModelState1d(linspace(-1, 1, 50),       # T
+                         linspace(25, -25, 50),     # φ
+                         ModelParams(1e-5, (0.04,), (50,)))
+    ζ = Iceberg.front_indices(state)
+    @test ζ == [26]
+
+    # multiple fronts
+    x = linspace(0, 4pi, 100)
+    state = ModelState1d(sin(x),       # T
+                         -sin(x),     # φ
+                         ModelParams(1e-5, (0.04,), (50,)))
+    ζ = Iceberg.front_indices(state)
+    @test ζ == [1, 26, 50, 75]
 end
 
 
@@ -29,5 +49,6 @@ end
 
 test_initialize1d()
 test_interp1d()
+test_front_indices()
 
 end #module
