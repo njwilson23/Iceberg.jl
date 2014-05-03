1ebb065dd2daec07ebba941da71c485392ce044d
Nat Wilson <njwilson23@gmail.com>
2014-05-02 23:59:58 -0400

    try moving includes up

--- src/Iceberg.jl
+++ src/Iceberg.jl
@@ -1,10 +1,12 @@
 module Iceberg
 
-export ModelParams, ModelState1d, ModelParams2d,
-       PhysicalParams
-
 include("heat.jl")
 include("ib_types.jl")
+
+export ModelParams,
+       ModelState1d,
+       ModelParams2d,
+       PhysicalParams
 
 # test problem with a step function in 1d
 function initialize1d_step(n=32)
