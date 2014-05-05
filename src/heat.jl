f158a2e4b49a5ea7a0d1c0991ff254573690f494
Nat Wilson <njwilson23@gmail.com>
2014-05-04 21:44:46 -0400

    tweak tsolve! for 2d
    tsolve! should basically work for nd. the assemble_mat function is limited to 2d
    for now. solutions look qualitatively reasonable, but require validation testing

--- src/heat.jl
+++ src/heat.jl
@@ -11,8 +11,6 @@ function assemble_mat(state::ModelState1d)
     n = state.params.nx[1]
     L = spdiagm((ones(n-1), -2ones(n), ones(n-1)), (-1, 0, 1))
     # Apply Dirichlet boundary conditions
-    # Warning: elementwise modification of sparse matrices is slow. This could
-    # be done during matrix assembly, at loss of clarity
     L[1,2] = 0.0
     L[end,end-1] = 0.0
     L[1,1] = 0.0
@@ -24,32 +22,33 @@ function assemble_mat(state::ModelState2d)
     m, n = state.params.nx
     L1 = spdiagm((ones(n-1), -2ones(n), ones(n-1)), (-1, 0, 1))
     L2 = spdiagm((ones(m-1), -2ones(m), ones(m-1)), (-1, 0, 1))
+    # Apply Dirichlet boundary conditions
     for L_ in (L1, L2)
         L_[1,2] = 0.0
         L_[end,end-1] = 0.0
         L_[1,1] = 0.0
         L_[end,end] = 0.0
     end
-    L = kron(L1, eye(m)) + kron(eye(n), L2)
+    L = kron(L1, speye(m)) + kron(speye(n), L2)
     return L
 end
 
 # solve the temperature evolution equation using a 2nd order in time and space
 # Crank-Nicolsen scheme
 function tsolve!(state::ModelState, phys::PhysicalParams)
+    n = prod(state.params.nx)
     L = assemble_mat(state)
 
     mskSolid = state.phi .> 0.0
 
-    alpha = Array(Float64, state.params.nx)
-    alpha[mskSolid] = phys.kaps / (phys.cps * phys.rhos)
-    alpha[!mskSolid] = phys.kapl / (phys.cpl * phys.rhol)
+    alpha = Array(Float64, n)
+    alpha[mskSolid[:]] = phys.kaps / (phys.cps * phys.rhos)
+    alpha[!mskSolid[:]] = phys.kapl / (phys.cpl * phys.rhol)
 
     # Solve (1 - 2/(2h^2)L) * Tnext = (1 + 2/(2h^2)) * T
-    n = state.params.nx[1]
     k2h2L = state.params.dt / (2*state.params.dx[1]^2) * L * spdiagm(alpha)
     state.temp[:] = (spdiagm(ones(n)) - k2h2L) \ 
-                    ((spdiagm(ones(n)) + k2h2L) * state.temp)
+                    ((spdiagm(ones(n)) + k2h2L) * state.temp[:])
 
     state.temp[~mskSolid] = max(state.temp[~mskSolid], phys.tmelt)
     state.temp[mskSolid] = min(state.temp[mskSolid], phys.tmelt)
